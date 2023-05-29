6ed5931164bbdc9694e32015c1cd85076bdec273
AnaLuciaBorges <gabriel.vscode@gmail.com>
2023-05-29 04:07:05 -0300

    add project 40%

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({ 
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server Running on http://localhost:3333')
+  })
---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import { getUser } from '@/lib/auth'
+import { User } from 'lucide-react'
+import Image from 'next/image'
+
+const GITHUB = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+
+export const Profile = () => {
+    const { name, avatarUrl } = getUser()
+    return (
+        <div className="flex items-center gap-3 text-left ">
+            <Image
+                src={avatarUrl}
+                width={40}
+                height={40}
+                alt=""
+                className="h-10 w-10 rounded-full"
+            />
+
+            <p className="max-w-[140px] text-sm font-bold leading-snug">
+                {name}
+                <a href="" className="hever:text-red-300 font-sans font-bold block text-red-400">
+                    Sair
+                </a>
+            </p>
+        </div>
+    )
+}
